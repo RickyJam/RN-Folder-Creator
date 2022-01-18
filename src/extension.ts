4a692bbd5a96598861a6cc60eaceaea1170a9a1f
riccardo.pacifico <riccardo.pacifico@trovaprezzi.it>
2022-01-18 14:27:10 +0100

    adesso stampiamo l'uri

--- src/extension.ts
+++ src/extension.ts
@@ -2,8 +2,9 @@ import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
 	
-	let disposable = vscode.commands.registerCommand('folder-creator.create', () => {
+	let disposable = vscode.commands.registerCommand('folder-creator.create', (uri: vscode.Uri) => {
 		vscode.window.showInformationMessage('Hai richiesto di creare una nuova folder!');
+		console.log(uri.fsPath);
 	});
 
 	context.subscriptions.push(disposable);
